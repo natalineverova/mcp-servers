bc4188a0823b00dd2f62289c32ad93ebb3b4fa50
Natali <neverova.natali@gmail.com>
2025-04-16 17:31:29 +0300

    Add solution for: создать резервную копию PostgreSQL (refs #36)

--- home/user/backup.sh
+++ home/user/backup.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# Specify the database parameters
-DB_HOST="your_db_host"
-DB_NAME="your_db_name"
-DB_USER="your_db_user"
-DB_PASSWORD="your_db_password"
+# Specify the source database parameters
+SOURCE_HOST="source_host"
+SOURCE_DB="source_db"
+SOURCE_USER="source_user"
+SOURCE_PASSWORD="source_password"
 
-# Specify the backup directory and filename
-BACKUP_DIR="/path/to/your/backup/directory"
-BACKUP_FILE="$BACKUP_DIR/backup_$(date +%Y%m%d%H%M%S).sql"
+# Specify the backup file path
+BACKUP_PATH="/path/to/your/backup/file.sql"
 
 # Export the PGPASSWORD environment variable
-export PGPASSWORD=$DB_PASSWORD
+export PGPASSWORD=$SOURCE_PASSWORD
 
-# Create a backup using pg_dump
-pg_dump -h $DB_HOST -U $DB_USER -d $DB_NAME -Fp -f $BACKUP_FILE
+# Use pg_dump to create a backup
+pg_dump -h $SOURCE_HOST -U $SOURCE_USER -F p -b -v -f $BACKUP_PATH $SOURCE_DB
 
 # Unset the PGPASSWORD environment variable
 unset PGPASSWORD
-
-echo "Backup created successfully at $BACKUP_FILE"
