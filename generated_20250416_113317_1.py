53a48ebda13c3caee4974c6a2a9619d513fafd44
Natali <neverova.natali@gmail.com>
2025-04-16 11:33:21 +0300

    Add solution for: Создать простую функцию для вывода приветствия

---
+++ generated_20250416_113317_1.py
@@ -0,0 +1,14 @@
+def greeting(name):
+    """
+    This function prints a greeting message.
+
+    Parameters:
+    name (str): The name of the person to greet.
+
+    Returns:
+    None
+    """
+    print(f"Hello, {name}!")
+
+# Test the function
+greeting("World")
